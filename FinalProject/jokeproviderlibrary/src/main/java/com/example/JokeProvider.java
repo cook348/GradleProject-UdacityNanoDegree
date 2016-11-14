ccef53e6a22ebdee40a3c2458f824577fd401a44
Daniel <cook348@gmail.com>
2016-11-14 15:25:44 -0500

    Create jokeproviderlibrary

---
+++ FinalProject/jokeproviderlibrary/src/main/java/com/example/JokeProvider.java
@@ -0,0 +1,9 @@
+package com.example;
+
+public class JokeProvider {
+    public String provideJoke(){
+        // Joke taken from: http://www.laughfactory.com/jokes/word-play-jokes
+        return "What happens to a frog's car when it breaks down?\n" +
+                "It gets toad away.";
+    }
+}
