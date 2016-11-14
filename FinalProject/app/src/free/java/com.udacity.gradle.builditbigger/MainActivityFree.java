e61de5d9067390d1b1edd269956120f8b945060e
Daniel <cook348@gmail.com>
2016-11-14 15:43:44 -0500

    Add free flavor

---
+++ FinalProject/app/src/free/java/com.udacity.gradle.builditbigger/MainActivityFree.java
@@ -0,0 +1,94 @@
+package com.udacity.gradle.builditbigger;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.cookapps.jokeactivitylibrary.DisplayJokeActivity;
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.InterstitialAd;
+
+
+public class MainActivityFree extends ActionBarActivity implements RetrieveJokesTask.RetrieveJokeListener {
+
+    InterstitialAd mInterstitialAd;
+    ProgressDialog mProgress;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mInterstitialAd = new InterstitialAd(this);
+        mInterstitialAd.setAdUnitId("ca-app-pub-3940256099942544/1033173712");
+
+        mInterstitialAd.setAdListener(new AdListener() {
+            @Override
+            public void onAdClosed() {
+                requestNewInterstitial();
+                startJokeTask();
+            }
+        });
+
+        requestNewInterstitial();
+
+    }
+
+    private void requestNewInterstitial() {
+        AdRequest adRequest = new AdRequest.Builder()
+                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+                .build();
+
+        mInterstitialAd.loadAd(adRequest);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void tellJoke(View view) {
+
+        if (mInterstitialAd.isLoaded()) {
+            mInterstitialAd.show();
+        } else {
+            startJokeTask();
+        }
+    }
+
+
+    private void startJokeTask(){
+        mProgress = ProgressDialog.show(this, getString(R.string.please_wait), getString(R.string.joke_loading));
+        new RetrieveJokesTask(this).execute();
+    }
+
+    @Override
+    public void onComplete(String joke) {
+        mProgress.dismiss();
+        Intent intent = new Intent(this, DisplayJokeActivity.class);
+        intent.putExtra(DisplayJokeActivity.EXTRA_JOKE, joke);
+        this.startActivity(intent);
+    }
+}
